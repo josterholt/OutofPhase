1a164df36bd100a08d548e338ed903d0dba9e94e
josterholt <josterholt@ubuntu.(none)>
2015-01-30 08:02:21 -0800

    Updating server side engine

--- app/server.py
+++ app/server.py
@@ -8,7 +8,64 @@ from tornado import websocket
 r = redis.StrictRedis(host='localhost', port=6379, db=10)
 session_id = 1234
 
+class GameState:
+    gameID = None
+    players = [{"x": 0, "y": 0}, {"x": 0, "y": 0 }]
+
+    def __init__(self):
+        print("Setting game id")
+        self.gameID = 1
+
+    def setPlayer(self, index, player):
+        self.players[index] = player
+
+    def updatePosition(self, indx, x, y):
+        self.players[indx]['x'] = x;
+        self.players[indx]['y'] = y;
+
+    def getPlayerPositions(self):
+        return self.players
+
 class EventMgr:
+    sessionid = None
+    game = None
+
+    def __init__(self, handler, game):
+        self.handler = handler
+        self.sessionid = handler.id
+        self.game = game
+
+        print(self.game.gameID)
+
+    # Create game
+    def createGame(self, request):
+        # Add to game list
+        unique_id = uuid.uuid4()
+        game_id = "game:" + str(unique_id)
+
+        if self.get_cookie("player") == 1:
+            self.gs.setPlayer(0, self)
+        elif self.get_cookie("player") == 2:
+            self.gs.setPlayer(1, self)
+        else:
+            print("Invalid player")
+
+
+        # Add to users personal game list
+        #r.rpush("games:" + str(self.sessionid), game_id)
+
+        # Initialize in-memory game variables
+        state = {
+            "x": 0,
+            "y": 0,
+            "velocity": {
+                "x": 0,
+                "y": 0
+            }
+        }
+        #r.hset(game_id, "player1", state)
+        #r.hset(game_id, "player2", {})
+
     # Retrieve game state of all players
     def getGameStates(self, request):
         game_states = []
@@ -30,9 +87,16 @@ class EventMgr:
         }
         return(game_state)
 
-    def updatePlayerPosition(selfself, request):
-        print(r.hset(session_id, "x", request.get("x")))
-        r.hset(session_id, "y", request.get("y"))
+
+    def updatePlayer(self, request):
+        print(self.handler.get_cookie("player"))
+        if(self.handler.get_cookie("player") == 1):
+            player_indx = 0
+        else:
+            player_indx = 1
+
+        self.game.updatePosition(player_indx, request.get("x"), request.get("y"))
+        return self.game.getPlayerPositions()
 
     # Sync all object states
     def syncAllObjects(self, request):
@@ -54,6 +118,8 @@ class EventMgr:
 # Store data in Memcache
 class GameClient(websocket.WebSocketHandler):
     clients = []
+    em = None
+    gs = GameState()
 
     def check_origin(self, origin):
         return True
@@ -62,29 +128,37 @@ class GameClient(websocket.WebSocketHandler):
         return "binary"
 
     def open(self):
-        self.id = uuid.uuid4()
+        game_session = self.get_cookie("game_session")
+        if game_session is None:
+            self.id = uuid.uuid4()
+        else:
+            self.set_cookie("game_session", self.id)
+
         self.clients.append(self)
+        print("Initial cookie")
+        print(self.get_cookie("player"))
 
-        # Add to game list
 
-        # Add players to variable
-        
+        self.em = EventMgr(self, self.gs)
         print("WebSocket opened")
 
     def on_message(self, message):
         response = {"status": None, "data": None}
         print(message)
-        request = json.loads(message)
 
-        em = EventMgr()
 
-        if "action" not in request or request.get("action") not in dir(em):
-            response = em.default(request)
-        else:
-            response['data'] = getattr(em, request.get("action"))(request)
+        try:
+            request = json.loads(message)
 
-        response['status'] = 'OK'
-        self.write_message(json.dumps(response))
+            if "action" not in request or request.get("action") not in dir(self.em):
+                response = self.em.default(request)
+            else:
+                response['data'] = getattr(self.em, request.get("action"))(request)
+
+            response['status'] = 'OK'
+            self.write_message(json.dumps(response))
+        except ValueError as e:
+            self.write_message(json.dumps({"error": "Bad Value"}))
 
 
 
